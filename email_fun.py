f046f880cfdccec4989043409dd2a24aa61777a6
Dante-Go <kafei_20123@163.com>
2018-12-19 15:38:32 +0800

    1.email with attachment

--- email_fun.py
+++ email_fun.py
@@ -1,20 +1,19 @@
 from email.mime.text import MIMEText
 import smtplib
+from email.mime.multipart import MIMEBase, MIMEMultipart
 
-mail_content = """
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <title>Title</title>
-        </head>
-        <body>
-        <h1> 这是一封HTML格式邮件</h1>
-        </body>
-        </html>
-        """
+mail_mul = MIMEMultipart()
 
-msg = MIMEText(mail_content, "html", "utf-8")
+mail_text = MIMEText('email with attachment', 'plain', 'utf-8')
+
+mail_mul.attach(mail_text)
+
+with open('hano.py', 'rb') as f:
+	s = f.read()
+	m = MIMEText(s, 'base64', 'utf-8')
+	m['Content-Type'] = 'application/octet-stream'
+	m['Content-Disposition'] = 'attachment; filename="hano.py"'
+	mail_mul.attach(m)
 
 from_addr = "2528448730@qq.com"
 from_token = "hgcmklipwrxvjfea"
@@ -26,7 +25,7 @@ smtp_srv = "smtp.qq.com"
 try:
 	srv = smtplib.SMTP_SSL(smtp_srv.encode(), 465) # smtp port: 25
 	srv.login(from_addr, from_token)
-	srv.sendmail(from_addr, [to_addr], msg.as_string())
+	srv.sendmail(from_addr, [to_addr], mail_mul.as_string())
 	srv.quit()
 except Exception as e:
 	print(e)
