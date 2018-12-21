6251899179ff24d193be9aea4ea64ddc606108c3
Dante-Go <kafei_20123@163.com>
2018-12-21 22:10:36 +0800

    User-Agent: headers=

---
+++ spider/urllib_user_agent_01.py
@@ -0,0 +1,21 @@
+from urllib import request, error
+
+if __name__ == '__main__':
+    url = 'http://www.baidu.com'
+
+    try:
+        headers = {}
+        headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'
+        req = request.Request(url, headers=headers)
+        print(req.headers)
+
+        rsp = request.urlopen(req)
+        html = rsp.read().decode()
+
+    except error.HTTPError as e:
+        print(e)
+    except error.URLError as e:
+        print(e)
+    except Exception as e:
+        print(e)
+    print('DONE..............')
