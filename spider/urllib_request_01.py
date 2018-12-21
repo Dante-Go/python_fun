f54763df1ae437f676d808815adf46e3a5392314
Dante-Go <kafei_20123@163.com>
2018-12-21 21:01:51 +0800

    get with parameter

--- spider/urllib_request_01.py
+++ spider/urllib_request_01.py
@@ -1,14 +1,26 @@
 from urllib import request
 import chardet
+from urllib import parse
 
 if __name__ == '__main__':
-	url = "http://www.csdn.net"
-	rsp = request.urlopen(url)
-	html = rsp.read()
+    url = 'http://www.baidu.com/s?'
+    wd = input("Input your keyword:")
 
-	cs = chardet.detect(html)
-	print(type(cs))
-	print(cs)
+    qs = {
+        "wd": wd
+    }
+    qs = parse.urlencode(qs)
+    print(qs)
 
-	html = html.decode(cs.get('encoding', 'utf-8'))
-	print(html)
+    fullurl = url + qs
+    print(fullurl)
+
+    rsp = request.urlopen(fullurl)
+    html = rsp.read()
+    cs = chardet.detect(html)
+    html = html.decode(cs.get('encoding', 'utf-8'))
+    print(html)
+    # print('URL: {0}'.format( rsp.geturl()))
+    # print('Info: {0}'.format( rsp.info()))
+    # print('Code: {0}'.format( rsp.getcode()))
+
