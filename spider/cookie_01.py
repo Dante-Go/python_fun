51b3eb29f6564ba4f85b78be7bec679e95193b0d
Dante-Go <kafei_20123@163.com>
2018-12-22 12:49:24 +0800

    get cookie via login

--- spider/cookie_01.py
+++ spider/cookie_01.py
@@ -1,13 +1,36 @@
-from urllib import request
+from urllib import request, parse
+from http import cookiejar
+
+cookie = cookiejar.CookieJar()
+
+cookie_handler = request.HTTPCookieProcessor(cookie)
+
+http_handler = request.HTTPHandler()
+
+https_handler = request.HTTPSHandler()
+
+opener = request.build_opener(http_handler, https_handler, cookie_handler)
+
+def login():
+    url = 'http://www.renren.com/PLogin.do'
+    data = {
+        'email': '13119144223',
+        'password': '123456'
+    }
+    data = parse.urlencode(data)
+
+    req = request.Request(url, data=data.encode())
+
+    rsp = opener.open(req)
+
+def getHomePage():
+    url = 'http://www.renren.com/965187997/profile'
+
+    rsp = opener.open(url)
+    html = rsp.read().decode()
+    with open('rsp.html', 'w', encoding='utf-8') as f:
+        f.write(html)
 
 if __name__ == '__main__':
-    url = 'http://www.renren.com/'
-
-    headers = {
-        'Cookie': 'anonymid=jpyxxtpg-xc3mq7; depovince=GW; _r01_=1; JSESSIONID=abcC34hG9JECc4X0UfuFw; ick_login=bf02c7fb-3898-4698-ab92-d03daa39e70e; first_login_flag=1; ln_uact=13119144223; ln_hurl=http://head.xiaonei.com/photos/0/0/men_main.gif; loginfrom=syshome; ch_id=10016; jebe_key=31ce5943-724a-4f4c-ad62-1d1e59c3b01f%7C7629e5c8e0f7f334244eb5436ef05a2c%7C1545452082030%7C1%7C1545452082017; wp_fold=0; jebecookies=2a6f27d6-3584-4d77-a649-35b2ef9124e6|||||; _de=420A8DC764CD1624FC7C8526DA9A3A25; p=0ef13bb3e4c83f5f69a11b398ee647ca7; t=d7968229333933f5892744639138a3237; societyguester=d7968229333933f5892744639138a3237; id=965187997; xnsid=2bde0602'
-    }
-    req = request.Request(url, headers=headers)
-    rsp = request.urlopen(req)
-
-    html = rsp.read().decode()
-    print(html)
+    login()
+    getHomePage()
