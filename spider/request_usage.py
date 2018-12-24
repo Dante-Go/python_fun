70d1724553ea91c73d4f45588867cd710ddf3bf7
Dante-Go <kafei_20123@163.com>
2018-12-24 11:56:08 +0800

    requests usage v2

--- spider/request_usage.py
+++ spider/request_usage.py
@@ -1,20 +1,18 @@
 import requests
+import json
 
-url = 'http://www.baidu.com/s?'
+baseUrl = 'https://fanyi.baidu.com/sug'
 
-kw = {
-	'wd': 'fury'
+data = {
+	'kw': 'fury'
 }
-
 headers = {
-	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36'
+	'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.119 Safari/537.36',
+	'Content-Length': str(len(data))
 }
 
-#rsp = requests.request('get', url)
-rsp = requests.get(url, params=kw, headers=headers)
+#rsp = requests.post(baseUrl, data=data, headers=headers, verify=False)
+rsp = requests.post(baseUrl, data=data, headers=headers)
 
 print(rsp.text)
-print(rsp.content)
-print(rsp.url)
-print(rsp.encoding)
-print(rsp.status_code)
+print(rsp.json())
