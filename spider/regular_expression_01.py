fdd067bc91e0586fdf6be9bbc3c1cb92aa9d8922
Dante-Go <kafei_20123@163.com>
2018-12-25 14:29:15 +0800

    regular expression: findall, finditer

--- spider/regular_expression_01.py
+++ spider/regular_expression_01.py
@@ -9,3 +9,14 @@ print(m.group())
 print(m.start(0))
 print(m.end(0))
 print(m.span(0))
+print('-----------------------------------------------------')
+s = pattern.findall('aaa 11 and 12 bb 33')
+print(s)
+
+s = pattern.finditer('aaa 11 and 22, bb 33')
+print(type(s))
+
+for i in s:
+	print(i.group())
+
+print('-----------------------------------------------------')
