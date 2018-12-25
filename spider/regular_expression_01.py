019cf77a5255c97199936f90835148935633319f
Dante-Go <kafei_20123@163.com>
2018-12-25 14:06:14 +0800

    regular expression; group; span;

---
+++ spider/regular_expression_01.py
@@ -0,0 +1,16 @@
+import re
+
+s = r'([a-z]+) ([a-z]+)'
+pattern = re.compile(s, re.I)
+
+m = pattern.match('hello world wide web')
+
+s = m.group(0)
+print(s)
+a = m.span(0)
+print(a)
+s = m.group(1)
+print(s)
+a = m.span(1)
+s = m.groups()
+print(s)
